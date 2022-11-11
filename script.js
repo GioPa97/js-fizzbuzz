8f3ef796c22015b858a063683479f38d0aa0606d
GioPa97 <giovannipagliara97@gmail.com>
2022-11-11 16:49:01 +0100

    Aggiunto sfondo

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-let containerPrincipale = document.querySelector(".mio-container");
 
-for (let i = 0; i < 1000; i++ ){
+let container=document.querySelector(".mio-container");
 
+/*Inserisco in un ciclo le variabili*/
 
-    switch(i){
-        case fizz:
-            containerPrincipale.innerHTML += '<div class="square"><p>Fizz</p></div>';
-            break;
-        case buzz:
-            containerPrincipale.innerHTML += '<div class="square"><p>Buzz</p></div>';
-            break;
-        case fizzbuzz:
-            containerPrincipale.innerHTML += '<div class="square"><p>FizzBuzz</p></div>';
-            break;
-        
-        default:
-            containerPrincipale.innerHTML += '<div class="square"><p>' + i + '</p></div>';
+for(let i=1; i<=100; i++){
+
+    if (i%3==0){
+        container.innerHTML += '<div class="card"><p>Fizz</p></div>';
+
+    } else if (i%3==0 && i%5==0){
+        container.innerHTML += '<div class="card"><p>FizzBuzz</p></div>';
+  
     }
+    else if(i%5==0){
+        container.innerHTML += '<div class="card"><p>Buzz</p></div>';
+    }
+    else{
+        container.innerHTML += '<div class="card"><p>'+i+'</p></div>';
+    }
+};
 
-}
