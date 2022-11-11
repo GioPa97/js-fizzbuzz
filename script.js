542b677d2717f6b7177628397b504706fd0e07b8
GioPa97 <giovannipagliara97@gmail.com>
2022-11-11 16:56:41 +0100

    Corretta fizzbuzz function

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
 
-let container=document.querySelector(".mio-container");
+let container = document.querySelector(".mio-container");
 
 /*Inserisco in un ciclo le variabili*/
 
-for(let i=1; i<=100; i++){
-
-    if (i%3==0){
-        container.innerHTML += '<div class="card"><p>Fizz</p></div>';
-
-    } else if (i%3==0 && i%5==0){
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
         container.innerHTML += '<div class="card"><p>FizzBuzz</p></div>';
-  
     }
-    else if(i%5==0){
+    else if (i % 3 == 0) {
+        container.innerHTML += '<div class="card"><p>Fizz</p></div>';
+    }
+    else if (i % 5 == 0) {
         container.innerHTML += '<div class="card"><p>Buzz</p></div>';
     }
-    else{
-        container.innerHTML += '<div class="card"><p>'+i+'</p></div>';
+    else {
+        container.innerHTML += '<div class="card"><p>' + i + '</p></div>';
     }
 };
-
